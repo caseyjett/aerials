2111beac4f4a454044f07ed3bb047f0fb317fb94
caseyjett <crjett01@gmail.com>
2021-03-28 12:13:35 -0400

    Fix database connection and delete excess

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express'); 
 const router = express.Router(); 
-const AerialTypes = require('../db/models/aerials').AerialTypes; 
-const Moves = require('../db/models/moves').Moves; 
+
+const { sequelize, models } = require('../db');
+const { AerialType, Moves } = models; 
+
 
 /* Handler function to wrap each route. */
 function asyncHandler(cb){
@@ -14,6 +16,7 @@ function asyncHandler(cb){
     }
 }
 
+//This should be deleted once the db is connected
 const exercises = [
     'warm-ups', 
     'trapeze', 
@@ -21,41 +24,34 @@ const exercises = [
     'conditioning'
 ]; 
 
-const trapezeMoves = [
-    'gazelle', 
-    'half angel', 
-    'mermaid on the bar', 
-    'mermaid under the bar'
-]
-
 //FIRST PRIORITY
 //Send a GET request to /aerials READ(view) a list of all exercise types and moves
 router.get('/aerials', asyncHandler(async (req, res) => {
+    //look below to sort out how to make the list on this page use the exercises in the AerialTypes db
     res.render('index', {exercises})
 }));
 
-//Send a GET request to /aerials/{$exercise} READ(view) specific exercise moves
-// router.get('/:exercise', (req, res) => {
-//     res.render('eachAerial', { trapezeMoves })
-//     you can use template literal in res.render(`/'aerials/${exercise}`) but will need to first define what that variable is somehow
-// })
+
+//Send a GET request to /aerials/{$exercise} READ(view) specific exercise type
+router.get('/aerials/:id', asyncHandler(async (req, res) => {
+  //Find the aerialType by its id and add it to the render variables
+  const exerciseList = await Moves.findAll({
+    where: {
+      exerciseTypeId: req.params.id} 
+    }); 
+  const trapezeMoves = exerciseList.map(move => move.move)
+  res.render('eachAerial', { trapezeMoves,  })
+})); 
+
+//Send a POST request to /aerials/:id to CREATE an individual move to the exercise type 
+
 
 //Send a POST request to /aerials to CREATE a new exercise type
 router.post('/aerials', asyncHandler(async (req, res) => {
-    const exerciseType = await AerialTypes.create(req.body); 
-    // console.log(req.body);
-    // res.cookie('exercise', req.body.exercise)
-    res.redirect('/aerials/' + exerciseType.id)
+  console.log(req.body);  
+  const exerciseType = await AerialType.create(req.body); 
+  res.redirect(`/aerials/${exerciseType.id}`)
 })); 
-//Send a PUT request to /aerials to UPDATE(edit) an exercise move
-
-
-//Send GET to /thanks to READ(view) a generic 'Thank you' for redirecting after adding an exercise or move
-router.get('/thanks', (req, res) => {
-    const exercise = req.cookies.exercise; 
-    res.render('thanks', {exercise})
-});
-
 
 
 
@@ -64,9 +60,9 @@ router.get('/thanks', (req, res) => {
 
 
 //SECOND PRIORITY
-//Send a GET request to / to READ a list of all excersise types
+//Send a PUT request to /aerials to UPDATE(edit) an exercise move
 
-//Send a GET request to /aerials/{$exercise}/:id to READ(view) a specific exercise move 
+//Send a GET request to /aerials/{$exercise}/:id to READ(view) a teh details of a specific exercise move 
 
 //Send a DELETE request to /aerials DELETE an exercise move
 
